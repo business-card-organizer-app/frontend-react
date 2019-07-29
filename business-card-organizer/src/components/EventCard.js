5253472ca25edb69aa2bf5377888133bd10ac2ad
Alex <agordon1123@gmail.com>
2019-07-29 16:16:58 -0400

    eventcard set up

--- business-card-organizer/src/components/EventCard.js
+++ business-card-organizer/src/components/EventCard.js
@@ -1,14 +1,36 @@
 import React from 'react';
+import styled from 'styled-components';
 
-const EventCard = (props) => {
+const StyledDiv = styled.div`
+    text-align: left;
+    margin: 5px;
+    padding: 20px;
+    border: 1px solid black;
+`;
+
+
+const EventCard = () => {
+    const props = {
+        "status": 201,
+        "data": [
+            {
+                "event_name": "Lambda Build Week",
+                "event_date": "22/8/2019",
+                "event_venue" : "Zoom",
+                "event_location": "Online",
+                "user_id" : 2,
+            }
+        ]
+    }    
+    
     return (
-        <div>
+        <StyledDiv>
             <h1>Event:</h1>
-            <h1>{props.event_name}</h1>
-            <p>{props.event_date}</p>
-            <p>{props.event_venue}</p>
-            <p>{props.event_location}</p>
-        </div>
+            <h1>{props.data[0].event_name}</h1>
+            <p>{props.data[0].event_date}</p>
+            <p>{props.data[0].event_venue}</p>
+            <p>{props.data[0].event_location}</p>
+        </StyledDiv>
     )
 }
 
