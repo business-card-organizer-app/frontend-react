cecd9a3d1410fd3a31d06419ad24a4d77a14f1f5
astillo <clomes.alex@gmail.com>
2019-07-30 18:49:22 -0500

    adding files

--- business-card-organizer/src/components/LargeCard.js
+++ business-card-organizer/src/components/LargeCard.js
@@ -5,8 +5,8 @@ import 'semantic-ui-css/semantic.min.css'
 const editButton = (<Button onClick={<Redirect to='/INSERT EDIT URL HERE' />}>Edit</Button>)
 
 const LargeCard = (props) => {
-
+    const { activeuser } = props.getActiveUser();
 }
-return (<div><Card header={props.first_name + props.last_name} meta={props.occupation} meta={props.phone} meta={props.email} description={props.tagline} image={props.qr_code} extra={editButton} />
+return (<div><Card header={activeuser.first_name + activeuser.last_name} meta={activeuser.occupation} meta={activeuser.phone} meta={activeuser.email} description={activeuser.tagline} image={activeuser.qr_code} extra={editButton} />
 </div>)
 export default LargeCard;
