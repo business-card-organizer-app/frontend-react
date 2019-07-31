32075cd8dbbe004c0c3e64532d8538faf94b65e6
Alex <agordon1123@gmail.com>
2019-07-31 17:12:02 -0400

    created CardShortList

---
+++ business-card-organizer/src/components/cardlist/CardShortList.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+const StyledCardShortList = styled.div`
+    border: 1px solid black;
+    max-width: 500px;
+    max-height: 500px;
+    overflow-y: scroll;
+`;
+
+const CardShortList = (props) => {
+    const { cardcollection } = props;
+    console.log(props);
+  
+    return (
+        <StyledCardShortList>
+            <h1>My Cards:</h1>
+
+            {cardcollection.map((card, i) => 
+                i < 3 ?
+                <SmallCardCreator
+                first_name={card.first_name}
+                last_name={card.last_name}
+                occupation={card.occupation}
+                phone={card.phone}
+                email={card.email}
+                tagline={card.tagline}
+                qr_code={card.qr_code}
+                />
+                : null
+            )}
+
+            <Link className='cards-button' to='/cards'>See all</Link>
+        </StyledCardShortList>
+    )
+}
+
+export default CardShortList;
