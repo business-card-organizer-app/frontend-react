2817b54b8fa6dca0f79b5b778ae948b26064c30a
maxdavid <me@maxdavid.com>
2019-08-01 17:21:53 -0700

    Fix card/event counting logic

--- business-card-organizer/src/components/events/EventCard.js
+++ business-card-organizer/src/components/events/EventCard.js
@@ -6,7 +6,7 @@ const StyledDiv = styled.div`
   margin: 5px;
   padding: 20px;
   box-shadow: 0 1px 3px 0 #d4d4d5, 0 0 0 1px #d4d4d5;
-  @media(min-width: 767px) {
+  @media (min-width: 767px) {
     margin-top: 15px;
   }
 `;
@@ -17,16 +17,16 @@ const StyledH = styled.h1`
 `;
 
 const StyledButtonPar = styled.button`
-  background-color: #AEAEAE;
+  background-color: #aeaeae;
   border: 1px solid black;
   border-radius: 5px;
   padding: 5px;
   width: 30%;
   text-align: center;
-  @media(min-width: 767px) {
+  @media (min-width: 767px) {
     width: 20%;
   }
-  @media(min-width: 991px) {
+  @media (min-width: 991px) {
     width: 15%;
   }
 `;
@@ -34,12 +34,13 @@ const StyledButtonPar = styled.button`
 const StyledLastPar = styled.p`
   border-bottom: 1px solid black;
   padding: 0 0 10px 0;
-  @media(min-width: 767px) {
+  @media (min-width: 767px) {
   }
 `;
 
 const EventCard = props => {
-  console.log('props', props)
+  console.log('props', props);
+  console.log(props.cardCollection);
   return (
     <StyledDiv>
       <StyledH>{props.name_event}</StyledH>
@@ -50,7 +51,7 @@ const EventCard = props => {
         Cards:{' '}
         {
           props.cardCollection.filter(
-            card => card.event_name === props.event_name
+            card => card.name_event === props.name_event
           ).length
         }
       </StyledButtonPar>
